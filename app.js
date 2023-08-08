a49de09bdaad4f4059d2b86c38f2609d03ee1a36
Shubham Rai <shubham56237@gmail.com>
2023-08-08 22:17:15 +0530

    added search bar functionalty

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client"
 import { FaSearchengin, FaUserSecret } from "react-icons/fa"
 
 const logo = <img className="logo" src={require("./img/logo.png")} />
-const searchIcon = <FaSearchengin className="search-icon"/>;
+const searchIcon = <FaSearchengin className="search-icon" onClick={onClickSearch}/>;
 const userIcon = <FaUserSecret size={24} color="white" />
-const searchBar = <input type="text" placeholder="Search Here"></input>
+const searchBar = <input type="text" placeholder="Search Here" ></input>
+
 const HeaderComponent = function () {
     return (
         <div className="header">
@@ -16,6 +17,19 @@ const HeaderComponent = function () {
         </div>
     )
 }
+
+// searchbar functionality
+// const searchIconJs = document.querySelector(".search-icon")
+function onClickSearch() {
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+    const rgb = `rgb(${r},${g},${b})`
+    const body = document.body
+    body.style.backgroundColor = rgb
+}
+// searchIconJs.addEventListener("click", onClickSearch)
+
 
 const rootJS = document.getElementById("root")
 const root = ReactDOM.createRoot(rootJS)
